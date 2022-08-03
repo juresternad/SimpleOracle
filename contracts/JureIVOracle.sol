a0f489695813998298d0759bcef4198bdbe64237
juresternad <sternad.jure@gmail.com>
2022-08-03 14:52:43 +0200

    weightedmedian

--- contracts/JureIVOracle.sol
+++ contracts/JureIVOracle.sol
@@ -1,7 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
+
 pragma solidity ^0.8.0;
 
 contract JureOracle {
+
+
+//====================================================================
+// Errors
+//====================================================================
+string internal constant NOT_OWNER = "You are not the owner";
+string internal constant ERR_COMMIT_NOT_ACTIVE = "Commit phase for chosen round not active";
+string internal constant ERR_REVEAL_NOT_ACTIVE = "Reveal phase for chosen round not active";
+string internal constant ERR_HASH_DOES_NOT_MATCH = "Hash doesn't match with vote and salt";
+string internal constant ERR_PREVIOUS_ROUND_STILL_ACTIVE = "Previous round still active";
+string internal constant ERR_ALREADY_VOTED = "You have already voted for this round";
 
 
 //====================================================================
@@ -14,36 +26,34 @@ struct Round {
     uint commitEndDate; // koncni cas za commit
     uint revealEndDate; // koncni cas za reveal
 
-    mapping (uint256 => Vote) votes; 
+    mapping (address => Vote) votes; 
 
     uint256 allWeight;
-    uint256[] prices;
-    uint256[] indexedVoters;
+    uint256[] indexedPrices;
+    uint256[] indexedWeights;
+    uint256[] index;
     
-    mapping (uint256 => uint256) changeWeights;
-
-    uint256 oraclePrice; // izracunana povprecje ustreznih glasov
-    uint256 oraclePowerPrice; // izracunano tehtano povprecje ustreznih glasov
+    uint256 weightedMedianPrice;
 } 
 
 struct Vote { 
     bytes32 voteHash;
     uint256 vote;
-    uint256 salt;
 }
 
-struct Voter {
-    uint256 weight; // njegova utez
-    uint256 currentWeight;
-}
+// struct Voter {
+//     uint256 weight; // njegova utez
+//     uint256 currentWeight;
+// }
 
-Voter[] public voters;
+address[] public voters;
 
-Voter[] public newVoters;
+// Voter[] public newVoters;
 
-mapping(address => uint256) public mapVoters;
+// mapping(address => uint256) public mapVoters;
 mapping(uint256 => Round) public rounds; // slovar, ki stevilo runde poveze z rundo
 
+mapping(address => uint256) weights;
 
 
 address private owner;
@@ -53,7 +63,7 @@ constructor() {
     }
 
 modifier onlyOwner() {
-    require(msg.sender == owner);
+    require(msg.sender == owner, NOT_OWNER);
     _;
 }
 
@@ -62,34 +72,28 @@ modifier onlyOwner() {
 // Commit/ Reveal
 //====================================================================
 
-function commitVote(bytes32 voteHash, uint256 vote, uint256 salt, uint256 roundNumber) public { // funkcija za commit
-    uint256 sender = mapVoters[msg.sender];
-    require(activeCommit(roundNumber),"Prepozno"); // preveri, ali je trenutno se odprt cas za glasovanje
-    require(rounds[roundNumber].votes[sender].voteHash == 0, "Ze glasoval"); // preveri ali je ze glasoval
-    rounds[currentRound].votes[sender] =Vote(
-            voteHash,
-            vote,
-            salt
-        );
+function commitVote(bytes32 _voteHash, uint256 roundNumber) public { // funkcija za commit
+    require(activeCommit(roundNumber),ERR_COMMIT_NOT_ACTIVE); // preveri, ali je trenutno se odprt cas za glasovanje
+    require(rounds[roundNumber].votes[msg.sender].voteHash == 0, ERR_ALREADY_VOTED); // preveri ali je ze glasoval
+    rounds[currentRound].votes[msg.sender].voteHash = _voteHash;
 }
     
 
-function revealVote(uint256 roundNumber) public {
-    require(activeReveal(roundNumber),"Prepozno");
-    uint256 sender = mapVoters[msg.sender];
+function revealVote(uint256 _vote, uint256 _salt, uint256 roundNumber) public {
+    require(activeReveal(roundNumber),ERR_REVEAL_NOT_ACTIVE);
 
-    bytes32 voteHash = rounds[roundNumber-1].votes[sender].voteHash;
-    uint256 salt = rounds[roundNumber-1].votes[sender].salt;
-    uint256 vote = rounds[roundNumber].votes[sender].vote;
+    bytes32 voteHash = rounds[roundNumber].votes[msg.sender].voteHash;
 
     if(currentRound>1){
-    require(keccak256(abi.encodePacked(vote, salt, msg.sender)) == voteHash,
-    "Se ne ujema"); 
+    require(keccak256(abi.encodePacked(_vote, _salt, msg.sender)) == voteHash,
+    ERR_HASH_DOES_NOT_MATCH); 
     }
-    rounds[roundNumber].allWeight += voters[sender].weight;
-
-    rounds[roundNumber].prices.push(vote);
-    rounds[roundNumber].indexedVoters.push(sender);
+    rounds[roundNumber].allWeight += weights[msg.sender];
+    rounds[roundNumber].votes[msg.sender].vote = _vote;
+    rounds[roundNumber].indexedPrices.push(_vote);
+    rounds[roundNumber].indexedWeights.push(weights[msg.sender]);
+    uint256 i = rounds[roundNumber].indexedPrices.length;
+    rounds[roundNumber].index.push(i);
 }
 
 
@@ -104,21 +108,8 @@ function hashh(uint256 ocena, uint256 salt) public view returns (bytes32 khash) 
 //====================================================================
 
 
-function startRound(uint256 commitTime, uint256 revealTime) public {
-    require (rounds[currentRound].revealEndDate <= block.timestamp); // Preveri, da je commitEndDate prejsnje runde manjsi od trenutnega casa.
-    uint256 n = voters.length;
-    for (uint i = 0; i < n; i++) {
-        if(rounds[currentRound].changeWeights[i] != 0)
-        {
-            voters[i].weight = rounds[currentRound].changeWeights[i];
-        }
-        voters[i].currentWeight = voters[i].weight;
-    }
-    uint256 m = newVoters.length;
-    for (uint i = 0; i < m; i++) {
-        voters.push(newVoters[i]);
-    }
-    delete newVoters;
+function startRound(uint256 commitTime, uint256 revealTime) public onlyOwner {
+    require (rounds[currentRound].revealEndDate <= block.timestamp, ERR_PREVIOUS_ROUND_STILL_ACTIVE); // Preveri, da je commitEndDate prejsnje runde manjsi od trenutnega casa.
     currentRound += 1;
     rounds[currentRound].commitEndDate = block.timestamp + commitTime * 1 seconds;
     rounds[currentRound].revealEndDate = rounds[currentRound].commitEndDate + revealTime * 1 seconds;
@@ -127,153 +118,168 @@ function startRound(uint256 commitTime, uint256 revealTime) public {
 
 
 function activeCommit (uint256 roundNumber) public view returns (bool active) {
-    require(block.timestamp <= rounds[roundNumber].commitEndDate); // Preveri, da je trenutni cas manjsi od casa commitEndDate v tej rundi.
+    if(block.timestamp <= rounds[roundNumber].commitEndDate) // Preveri, da je trenutni cas manjsi od casa commitEndDate v tej rundi.
     return true; 
 }
 
 
 function activeReveal (uint256 roundNumber) public view returns (bool active) {
-    require(rounds[roundNumber].commitEndDate <= block.timestamp); //
-    require(block.timestamp <= rounds[roundNumber].revealEndDate);  // Preveri, da je trenutni cas manjsi od casa revealEndDate v tej rundi.
+    if(rounds[roundNumber].commitEndDate <= block.timestamp) //
+    if(block.timestamp <= rounds[roundNumber].revealEndDate)  // Preveri, da je trenutni cas manjsi od casa revealEndDate v tej rundi.
     return true;
 }
-
-
-                                          
-                                                                                             
-                                                                                                
-//====================================================================
-// OraclePrice (roundNumber)
-//====================================================================
-
-
-function oraclePriceByRoundNumber (uint roundNumber) public view returns(uint256 price) {
-    uint256 n = voters.length;
-    uint256 computedPrice = 0;
-    for (uint i = 0; i < n; i++) {
-        computedPrice += rounds[roundNumber].votes[i].vote * voters[i].weight;
-    }
-    computedPrice = computedPrice/rounds[roundNumber].allWeight;
-    return computedPrice;
-
-}
-
-function oraclePriceByPowerByRoundNumber (uint roundNumber) public returns(uint256 price) {
-    uint256 n = voters.length;
-    uint256 quarter = rounds[roundNumber].allWeight /4;
-    uint256 newWeight = rounds[roundNumber].allWeight - 2 * quarter;
-    uint256 downBound = quarter;
-    uint256 upBound = quarter; 
-    uint256[] memory sortedVoters = sort(rounds[roundNumber].prices, rounds[roundNumber].indexedVoters);
-    uint256 i = 0;
-    uint256 j = n-1;
-    while (downBound != 0) {
-        uint256 voterOfThisPrice = sortedVoters[i];
-        uint256 PowerOfThisVoter = voters[voterOfThisPrice].weight;
-        if (PowerOfThisVoter >= downBound) {
-            voters[voterOfThisPrice].currentWeight = PowerOfThisVoter - downBound;
-            downBound = 0;
-        }
-        else {
-            voters[voterOfThisPrice].currentWeight = 0;
-            downBound -= PowerOfThisVoter;
-            i +=1;
-        }
-
-    }
-    while (upBound != 0) {
-        uint256 voterOfThisPrice = sortedVoters[j];
-        uint256 PowerOfThisVoter = voters[voterOfThisPrice].weight;
-        if (PowerOfThisVoter >= downBound) {
-            voters[voterOfThisPrice].currentWeight = PowerOfThisVoter - upBound;
-            upBound = 0;
-        }
-        else {
-            voters[voterOfThisPrice].currentWeight = 0;
-            upBound -= PowerOfThisVoter;
-            j -= 1;
-        }   
-
-    }
-    uint256 computedPowerPrice = 0;
-    for (uint k = 0; k < n; k++) {
-        computedPowerPrice += rounds[roundNumber].votes[k].vote * voters[k].currentWeight;
-    }
-    computedPowerPrice = computedPowerPrice/newWeight;
-    rounds[roundNumber].oraclePowerPrice = computedPowerPrice;
-    return computedPowerPrice;
-
-}
-
 
 //====================================================================
 // Voters
 //====================================================================
 
-function addVoter (address voter, uint256 weight) public onlyOwner {
-    require(mapVoters[voter] == 0, "Voter already exists");
-    uint256 i = voters.length;
-    newVoters.push(Voter(weight, weight));
-    mapVoters[voter] = i;
-}
-
-function changeWeight (uint256 voterIndex, uint256 weight) public onlyOwner {
-    require(voters.length >= voterIndex, "Voter doesn't exist");
-    rounds[currentRound].changeWeights[voterIndex]= weight;
-}
-                                 
-//====================================================================
-// Quicksort
-//====================================================================
-// sposojeno iz https://ethereum.stackexchange.com/a/1518
-
-function quickSort(uint256[] memory _prices, uint256[] memory _indexedVoters, int left, int right) public {
-    int i = left;
-    int j = right;
-    if (i == j) return;
-    uint256 pivot = _prices[uint256(left + (right - left) / 2)];
-    while (i <= j) {
-        while (_prices[uint256(i)] < pivot) i++;
-        while (pivot < _prices[uint256(j)]) j--;
-        if (i <= j) {
-            (_prices[uint256(i)], _prices[uint256(j)]) = (_prices[uint256(j)], _prices[uint256(i)]);
-            (_indexedVoters[uint256(i)], _indexedVoters[uint256(j)]) = (_indexedVoters[uint256(j)], _indexedVoters[uint256(i)]);
-            i++;
-            j--;
+function addOrUpdate (address voter, uint256 weight) public onlyOwner {
+    if(weight == 0){
+        weights[voter] = 0;
+        for (uint256 i; i<voters.length; i++) {
+            if (voters[i] == voter) {
+                voters[i] = voters[voters.length - 1];
+                voters.pop();
+                break;
+            }
         }
     }
-    if (left < j)
-        quickSort(_prices, _indexedVoters, left, j);
-    if (i < right)
-        quickSort(_prices, _indexedVoters, i, right);
+    if(weights[voter] != 0) {
+        weights[voter] = weight;
+    }
+    else {
+        voters.push(voter);
+        weights[voter] = weight;
+    }
+
+}                           
+
+//====================================================================
+// WeightedMedian
+//====================================================================
+
+struct Vars {            
+    uint256 leftSum;            
+    uint256 rightSum;           
+    uint256 newLeftSum;         
+    uint256 newRightSum;        
+    uint256 pivotWeight;        
+    uint256 pos;                
+    uint256 left;               
+    uint256 right;              
+    uint256 pivotId;            
+    uint256 leftMedianWeight;   
+    uint256 rightMedianWeight; 
 }
 
-function sort(uint256[] memory dataPrices, uint256[] memory dataVoters) public returns (uint256[] memory) {
-    quickSort(dataPrices, dataVoters, int(0), int(dataPrices.length - 1));
-    return (dataVoters);
+function weightedMedian() public returns (uint256 weightedMedianPrice){
+    if(rounds[currentRound].weightedMedianPrice != 0) {
+        return rounds[currentRound].weightedMedianPrice;
+    }
+    uint256 count = rounds[currentRound].indexedPrices.length;
+    for (uint256 i = 0; i < count; i++) {
+        rounds[currentRound].index[i] = i;
+    }
+    (uint256 medianIndex, uint256 leftSum, uint256 rightSum) = modifiedQuickSelect(
+        0,
+        count - 1,
+        0,
+        0
+    );
+    uint256 medianWeight = rounds[currentRound].indexedWeights[rounds[currentRound].index[medianIndex]];
+    uint256 totalSum = medianWeight + leftSum + rightSum;
+    uint256 finalMedianPrice = rounds[currentRound].indexedPrices[rounds[currentRound].index[medianIndex]];
+    if (leftSum + medianWeight == totalSum / 2 && totalSum % 2 == 0) {
+        finalMedianPrice =
+            (finalMedianPrice + rounds[currentRound].indexedPrices[rounds[currentRound].index[medianIndex + 1]]) / 2;
+    }
+    return finalMedianPrice;
+}
+
+function modifiedQuickSelect(
+    uint256 start,
+    uint256 end,
+    uint256 leftSumInit,
+    uint256 rightSumInit
+    )
+    internal view returns (uint256, uint256, uint256)
+    {
+    if (start == end) {
+        return (start, leftSumInit, rightSumInit);
+    }
+    Vars memory s;
+    s.leftSum = leftSumInit;
+    s.rightSum = rightSumInit;
+    s.left = start;
+    s.right = end;
+    uint256 random = uint256(keccak256(abi.encode(block.difficulty, block.timestamp)));
+    uint256 totalSum; 
+    while (true) {
+        (s.pos,s.newLeftSum,s.newRightSum) = partition(
+            s.left,
+            s.right,
+            (random % (s.right - s.left + 1)) + s.left,
+            s.leftSum,
+            s.rightSum
+        );
+        
+        s.pivotId = rounds[currentRound].index[s.pos];
+        s.pivotWeight = rounds[currentRound].indexedWeights[s.pivotId];
+        totalSum = s.pivotWeight + s.newLeftSum + s.newRightSum;
+
+        s.leftMedianWeight = totalSum / 2 + (totalSum % 2);  
+        s.rightMedianWeight = totalSum - s.leftMedianWeight; 
+        if (s.newLeftSum >= s.leftMedianWeight && s.leftMedianWeight > s.leftSum) { 
+            s.right = s.pos - 1;
+            s.rightSum = s.pivotWeight + s.newRightSum;
+        } else if (s.newRightSum > s.rightMedianWeight && s.rightMedianWeight > s.rightSum) {
+            s.left = s.pos + 1;
+            s.leftSum = s.pivotWeight + s.newLeftSum;
+        } else {
+            return (s.pos, s.newLeftSum, s.newRightSum);
+        }
+
+    }
+
+    return (0, 0, 0);
+}
+
+function partition(
+    uint256 left0,
+    uint256 right0,
+    uint256 pivotId,
+    uint256 leftSum0, 
+    uint256 rightSum0
+
+)
+    internal view returns (uint256, uint256, uint256)
+{
+    uint256[] memory sums = new uint256[](2);
+    sums[0] = leftSum0;
+    sums[1] = rightSum0;
+    uint256 left = left0;
+    uint256 right = right0;
+    uint256 pivotValue = rounds[currentRound].indexedPrices[rounds[currentRound].index[pivotId]];
+    swap(pivotId, right, rounds[currentRound].index);
+    uint256 storeIndex = left;
+    for (uint256 i = left; i < right; i++) {
+        uint256 eltId = rounds[currentRound].index[i];
+        if (rounds[currentRound].indexedPrices[eltId] < pivotValue) {
+            sums[0] += rounds[currentRound].indexedWeights[eltId];
+            swap(storeIndex, i, rounds[currentRound].index);
+            storeIndex++;
+        } else {
+            sums[1] += rounds[currentRound].indexedWeights[eltId];
+        }
+    }
+    swap(right, storeIndex, rounds[currentRound].index);
+    return (storeIndex, sums[0], sums[1]);
+}
+
+function swap(uint256 i, uint256 j, uint256[] memory index) internal pure {
+    if (i == j) return;
+    (index[i], index[j]) = (index[j], index[i]);
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 }
-
-
-
-
-
-
-
-
-
-                                   
