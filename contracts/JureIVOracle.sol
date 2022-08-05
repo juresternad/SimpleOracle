9d7d3ff47d311374f2394ea6b3ddb7104075c451
juresternad <sternad.jure@gmail.com>
2022-08-05 19:02:20 +0200

    komentarji 3. del

--- contracts/JureIVOracle.sol
+++ contracts/JureIVOracle.sol
@@ -79,28 +79,28 @@ contract JureIVOracle {
 
     /**
      * @notice Adds, updates voter's weights or deletes the voter
-     * @param voter                 Voter's address
-     * @param weight                New weight
+     * @param _voter                 Voter's address
+     * @param _weight                New weight
      */
-    function addOrUpdate(address voter, uint128 weight) public onlyOwner {
-        if (weight == 0) {
+    function addOrUpdate(address _voter, uint128 _weight) public onlyOwner {
+        if (_weight == 0) {
             // case of deleting the voter
-            weights[voter] = 0;
+            weights[_voter] = 0;
             for (uint256 i; i < voters.length; i++) {
-                if (voters[i] == voter) {
+                if (voters[i] == _voter) {
                     voters[i] = voters[voters.length - 1];
                     voters.pop();
                     return;
                 }
             }
         }
-        if (weights[voter] != 0) {
+        if (weights[_voter] != 0) {
             // case of updating the existing voter's weight
-            weights[voter] = weight;
+            weights[_voter] = _weight;
         } else {
             // case of adding a new voter
-            voters.push(voter);
-            weights[voter] = weight;
+            voters.push(_voter);
+            weights[_voter] = _weight;
         }
     }
 
@@ -110,66 +110,66 @@ contract JureIVOracle {
 
     /**
      * @notice Starts a new round
-     * @param commitTime              Duration of the commit phase
-     * @param revealTime              Duration of the reveal phase (starts "immediately" after the commit phase)
+     * @param _commitTime              Duration of the commit phase
+     * @param _revealTime              Duration of the reveal phase (starts "immediately" after the commit phase)
      */
-    function startRound(uint256 commitTime, uint256 revealTime) public onlyOwner {
+    function startRound(uint256 _commitTime, uint256 _revealTime) public onlyOwner {
         // Previous round's reveal phase must have ended
         require(rounds[currentRound].revealEndDate <= block.timestamp, ERR_PREVIOUS_ROUND_STILL_ACTIVE);
         currentRound += 1;
         // Sets the end dates
-        rounds[currentRound].commitEndDate = block.timestamp + commitTime * 1 seconds;
-        rounds[currentRound].revealEndDate = rounds[currentRound].commitEndDate + revealTime * 1 seconds;
+        rounds[currentRound].commitEndDate = block.timestamp + _commitTime * 1 seconds;
+        rounds[currentRound].revealEndDate = rounds[currentRound].commitEndDate + _revealTime * 1 seconds;
     }
 
     /**
      * @notice Checks whether the round's commit phase is active
-     * @param roundNumber              Number of the round
-     * @return active                  Boolean indicating whether the round's commit phase is active
+     * @param _roundNumber              Number of the round
+     * @return _active                  Boolean indicating whether the round's commit phase is active
      */
-    function activeCommit(uint256 roundNumber) public view returns (bool active) {
-        if (block.timestamp <= rounds[roundNumber].commitEndDate) return true;
+    function activeCommit(uint256 _roundNumber) public view returns (bool _active) {
+        if (block.timestamp <= rounds[_roundNumber].commitEndDate) return true;
     }
 
     /**
      * @notice Checks whether the round's reveal phase is active
-     * @param roundNumber              Number of the round
-     * @return active                  Boolean indicating whether the round's reveal phase is active
+     * @param _roundNumber              Number of the round
+     * @return _active                  Boolean indicating whether the round's reveal phase is active
      */
-    function activeReveal(uint256 roundNumber) public view returns (bool active) {
-        if (rounds[roundNumber].commitEndDate <= block.timestamp)
-        if (block.timestamp <= rounds[roundNumber].revealEndDate)
+    function activeReveal(uint256 _roundNumber) public view returns (bool _active) {
+        if (rounds[_roundNumber].commitEndDate <= block.timestamp)
+        if (block.timestamp <= rounds[_roundNumber].revealEndDate)
         return true;
     }
 
     /**
      * @notice Returns the weightedMedianPrice of the round
-     * @param roundNumber              Number of the round
-     * @return price                   Weighted median price of the round
+     * @param _roundNumber              Number of the round
+     * @return _price                   Weighted median price of the round
      */
-    function getPrice(uint256 roundNumber) public returns (uint256 price) {
-        if (rounds[roundNumber].revealedVoters.length == 0) {
+    function getPrice(uint256 _roundNumber) public returns (uint256 _price) {
+        if (rounds[_roundNumber].revealedVoters.length == 0) {
             // case of 0 successfully revealed votes
             return 0;
         }
-        if (rounds[roundNumber].weightedMedianPrice == 0) {
+        if (rounds[_roundNumber].weightedMedianPrice == 0) {
             // case of weightedMedianPrice of the round not yet been computed
-            uint256 count = rounds[roundNumber].revealedVoters.length;
+            uint256 count = rounds[_roundNumber].revealedVoters.length;
             // prepares the memory arrays
             uint256[] memory index = new uint256[](count);
             uint256[] memory indexedPrices = new uint256[](count);
             uint256[] memory indexedWeights = new uint256[](count);
             for (uint256 i = 0; i < count; i++) {
                 index[i] = i;
-                address revealedVoter = rounds[roundNumber].revealedVoters[i];
-                indexedPrices[i] = rounds[roundNumber] .votes[revealedVoter] .vote;
-                indexedWeights[i] = rounds[roundNumber] .votes[revealedVoter] .weight;
+                address revealedVoter = rounds[_roundNumber].revealedVoters[i];
+                indexedPrices[i] = rounds[_roundNumber] .votes[revealedVoter] .vote;
+                indexedWeights[i] = rounds[_roundNumber] .votes[revealedVoter] .weight;
             }
             // uses finalizePrice and updates the round's weightedMedianPrice
-            price = finalizePrice(count, index, indexedPrices, indexedWeights);
-            rounds[roundNumber].weightedMedianPrice = price;
+            _price = _finalizePrice(count, index, indexedPrices, indexedWeights);
+            rounds[_roundNumber].weightedMedianPrice = _price;
         }
-        return rounds[roundNumber].weightedMedianPrice;
+        return rounds[_roundNumber].weightedMedianPrice;
     }
 
     //====================================================================
@@ -179,15 +179,15 @@ contract JureIVOracle {
     /**
      * @notice Commits voter's hash
      * @param _voteHash                Voter's hash of the vote
-     * @param roundNumber              Number of the round
+     * @param _roundNumber              Number of the round
      */
-    function commitVote(bytes32 _voteHash, uint256 roundNumber) public {
+    function commitVote(bytes32 _voteHash, uint256 _roundNumber) public {
         // Checks round's commit-phase activity
-        require(activeCommit(roundNumber), ERR_COMMIT_NOT_ACTIVE);
+        require(activeCommit(_roundNumber), ERR_COMMIT_NOT_ACTIVE);
         // Prevents non-voters from commiting the hash
         require(weights[msg.sender] != 0, ERR_COMMIT_NO_WEIGHT);
         // Prevents double-commit
-        require(rounds[roundNumber].votes[msg.sender].voteHash == 0, ERR_ALREADY_VOTED);
+        require(rounds[_roundNumber].votes[msg.sender].voteHash == 0, ERR_ALREADY_VOTED);
         // Sets voter's hash in the round
         rounds[currentRound].votes[msg.sender].voteHash = _voteHash;
     }
@@ -196,25 +196,21 @@ contract JureIVOracle {
      * @notice Reveals voter's hash
      * @param _vote                    Voter's vote
      * @param _salt                    Salt he used of the hash
-     * @param roundNumber              Number of the round
+     * @param _roundNumber              Number of the round
      */
-    function revealVote(
-        uint256 _vote,
-        uint256 _salt,
-        uint256 roundNumber
-    ) public {
+    function revealVote(uint256 _vote, uint256 _salt, uint256 _roundNumber) public {
         // Checks round's reveal-phase activity
-        require(activeReveal(roundNumber), ERR_REVEAL_NOT_ACTIVE);
+        require(activeReveal(_roundNumber), ERR_REVEAL_NOT_ACTIVE);
         // Finds the voter's hash
-        bytes32 voteHash = rounds[roundNumber].votes[msg.sender].voteHash;
+        bytes32 voteHash = rounds[_roundNumber].votes[msg.sender].voteHash;
         // Checks matching of the vote's hash, vote, salt and msg.sender
         require(keccak256(abi.encodePacked(_vote, _salt, msg.sender)) == voteHash, ERR_HASH_DOES_NOT_MATCH);
         // Updates voter's vote, weight and hash in the round
-        rounds[roundNumber].votes[msg.sender].vote = _vote;
-        rounds[roundNumber].votes[msg.sender].weight = weights[msg.sender];
-        rounds[roundNumber].votes[msg.sender].voteHash = 0;
+        rounds[_roundNumber].votes[msg.sender].vote = _vote;
+        rounds[_roundNumber].votes[msg.sender].weight = weights[msg.sender];
+        rounds[_roundNumber].votes[msg.sender].voteHash = 0;
         // Adds voter to the revealedVoters array of the round
-        rounds[roundNumber].revealedVoters.push(msg.sender);
+        rounds[_roundNumber].revealedVoters.push(msg.sender);
     }
 
     // ====================================================================
@@ -248,97 +244,97 @@ contract JureIVOracle {
     
     /**
      * @notice Reveals voter's hash
-     * @param index                    Permutation of indices of the input arrays that determines the sorting of indexed prices
-     * @param indexedPrices            Positional memory array of indexed prices
-     * @param indexedWeights           Positional memory array of indexed weights
+     * @param _index                    Permutation of indices of the input arrays that determines the sorting of indexed prices
+     * @param _indexedPrices            Positional memory array of indexed prices
+     * @param _indexedWeights           Positional memory array of indexed weights
      * @return price                   Weighted median price of the round
      */
-    function finalizePrice(
+    function _finalizePrice(
         uint256 count,
-        uint256[] memory index,
-        uint256[] memory indexedPrices,
-        uint256[] memory indexedWeights
+        uint256[] memory _index,
+        uint256[] memory _indexedPrices,
+        uint256[] memory _indexedWeights
     ) internal view returns (uint256 price) {
-        Data memory data;
+        Data memory _data;
         // Performs modified quick select algorithm
-        (data.medianIndex, data.leftSum, data.rightSum) = modifiedQuickSelect(
+        (_data.medianIndex, _data.leftSum, _data.rightSum) = _modifiedQuickSelect(
             0,
             count - 1,
             0,
             0,
-            index,
-            indexedPrices,
-            indexedWeights
+            _index,
+            _indexedPrices,
+            _indexedWeights
         );
-        data.medianWeight = indexedWeights[index[data.medianIndex]];
-        uint256 totalSum = data.medianWeight + data.leftSum + data.rightSum;
-        data.finalMedianPrice = indexedPrices[index[data.medianIndex]];
-        if (data.leftSum + data.medianWeight == totalSum / 2 && totalSum % 2 == 0) {
+        _data.medianWeight = _indexedWeights[_index[_data.medianIndex]];
+        uint256 totalSum = _data.medianWeight + _data.leftSum + _data.rightSum;
+        _data.finalMedianPrice = _indexedPrices[_index[_data.medianIndex]];
+        if (_data.leftSum + _data.medianWeight == totalSum / 2 && totalSum % 2 == 0) {
             // Case of the median price being in the middle between two prices
             // Takes their awerage
-            data.finalMedianPrice = (data.finalMedianPrice + indexedPrices[index[data.medianIndex + 1]]) / 2;
+            _data.finalMedianPrice = (_data.finalMedianPrice + _indexedPrices[_index[_data.medianIndex + 1]]) / 2;
         }
-        return data.finalMedianPrice;
+        return _data.finalMedianPrice;
     }
     
     /**
      * @notice Performs modified quick select algorithm
      */
-    function modifiedQuickSelect(
-        uint256 start,
-        uint256 end,
-        uint256 leftSumInit,
-        uint256 rightSumInit,
-        uint256[] memory index,
-        uint256[] memory indexedPrices,
-        uint256[] memory indexedWeights
+    function _modifiedQuickSelect(
+        uint256 _start,
+        uint256 _end,
+        uint256 _leftSumInit,
+        uint256 _rightSumInit,
+        uint256[] memory _index,
+        uint256[] memory _indexedPrices,
+        uint256[] memory _indexedWeights
     )
         internal view returns ( uint256, uint256, uint256 )
     {
-        if (start == end) {
-            return (start, leftSumInit, rightSumInit);
+        if (_start == _end) {
+            return (_start, _leftSumInit, _rightSumInit);
         }
-        Variables memory vars;
-        Positions memory pos;
+        Variables memory _vars;
+        Positions memory _pos;
 
-        vars.leftSum = leftSumInit;
-        vars.rightSum = rightSumInit;
-        pos.left = start;
-        pos.right = end;
+        _vars.leftSum = _leftSumInit;
+        _vars.rightSum = _rightSumInit;
+        _pos.left = _start;
+        _pos.right = _end;
         uint256 random = uint256(
             keccak256(abi.encode(block.difficulty, block.timestamp))
         );
         uint256 totalSum;
         while (true) {
-            (pos.pos, vars.newLeftSum, vars.newRightSum) = partition(
-                pos.left,
-                pos.right,
-                (random % (pos.right - pos.left + 1)) + pos.left,
-                vars.leftSum,
-                vars.rightSum,
-                index,
-                indexedPrices,
-                indexedWeights
+            (_pos.pos, _vars.newLeftSum, _vars.newRightSum) = _partition(
+                _pos.left,
+                _pos.right,
+                (random % (_pos.right - _pos.left + 1)) + _pos.left,
+                _vars.leftSum,
+                _vars.rightSum,
+                _index,
+                _indexedPrices,
+                _indexedWeights
             );
 
-            pos.pivotId = index[pos.pos];
-            vars.pivotWeight = indexedWeights[pos.pivotId];
-            totalSum = vars.pivotWeight + vars.newLeftSum + vars.newRightSum;
+            _pos.pivotId = _index[_pos.pos];
+            _vars.pivotWeight = _indexedWeights[_pos.pivotId];
+            totalSum = _vars.pivotWeight + _vars.newLeftSum + _vars.newRightSum;
 
-            vars.leftMedianWeight = totalSum / 2 + (totalSum % 2);
-            vars.rightMedianWeight = totalSum - vars.leftMedianWeight;
+            _vars.leftMedianWeight = totalSum / 2 + (totalSum % 2);
+            _vars.rightMedianWeight = totalSum - _vars.leftMedianWeight;
             if (
-                vars.newLeftSum >= vars.leftMedianWeight && vars.leftMedianWeight > leftSumInit
+                _vars.newLeftSum >= _vars.leftMedianWeight && _vars.leftMedianWeight > _leftSumInit
             ) {
-                pos.right = pos.pos - 1;
-                vars.rightSum = vars.pivotWeight + vars.newRightSum;
+                _pos.right = _pos.pos - 1;
+                _vars.rightSum = _vars.pivotWeight + _vars.newRightSum;
             } else if (
-                vars.newRightSum > vars.rightMedianWeight && vars.rightMedianWeight > rightSumInit
+                _vars.newRightSum > _vars.rightMedianWeight && _vars.rightMedianWeight > _rightSumInit
             ) {
-                pos.left = pos.pos + 1;
-                vars.leftSum = vars.pivotWeight + vars.newLeftSum;
+                _pos.left = _pos.pos + 1;
+                _vars.leftSum = _vars.pivotWeight + _vars.newLeftSum;
             } else {
-                return (pos.pos, vars.newLeftSum, vars.newRightSum);
+                return (_pos.pos, _vars.newLeftSum, _vars.newRightSum);
             }
         }
 
@@ -349,44 +345,44 @@ contract JureIVOracle {
     /**
      * @notice Partitions the memory array index according to the pivot
      */
-    function partition(
-        uint256 left0,
-        uint256 right0,
-        uint256 pivotId,
-        uint256 leftSum0,
-        uint256 rightSum0,
-        uint256[] memory index,
-        uint256[] memory indexedPrices,
-        uint256[] memory indexedWeights
+    function _partition(
+        uint256 _left0,
+        uint256 _right0,
+        uint256 _pivotId,
+        uint256 _leftSum0,
+        uint256 _rightSum0,
+        uint256[] memory _index,
+        uint256[] memory _indexedPrices,
+        uint256[] memory _indexedWeights
     )
         internal pure returns ( uint256, uint256, uint256 )
     {
-        uint256 pivotValue = indexedPrices[index[pivotId]];
+        uint256 pivotValue = _indexedPrices[_index[_pivotId]];
         uint256[] memory sums = new uint256[](2);
-        sums[0] = leftSum0;
-        sums[1] = rightSum0;
-        uint256 left = left0;
-        uint256 right = right0;
-        swap(pivotId, right, index);
+        sums[0] = _leftSum0;
+        sums[1] = _rightSum0;
+        uint256 left = _left0;
+        uint256 right = _right0;
+        _swap(_pivotId, right, _index);
         uint256 storeIndex = left;
         for (uint256 i = left; i < right; i++) {
-            uint256 eltId = index[i];
-            if (indexedPrices[eltId] < pivotValue) {
-                sums[0] += indexedWeights[eltId];
-                swap(storeIndex, i, index);
+            uint256 eltId = _index[i];
+            if (_indexedPrices[eltId] < pivotValue) {
+                sums[0] += _indexedWeights[eltId];
+                _swap(storeIndex, i, _index);
                 storeIndex++;
             } else {
-                sums[1] += indexedWeights[eltId];
+                sums[1] += _indexedWeights[eltId];
             }
         }
-        swap(right, storeIndex, index);
+        _swap(right, storeIndex, _index);
         return (storeIndex, sums[0], sums[1]);
     }
     /**
      * @notice Swaps indices `i` and `j` in the memory array index
      */
-    function swap(uint256 i, uint256 j, uint256[] memory index) internal pure {
+    function _swap(uint256 i, uint256 j, uint256[] memory _index) internal pure {
         if (i == j) return;
-        (index[i], index[j]) = (index[j], index[i]);
+        (_index[i], _index[j]) = (_index[j], _index[i]);
     }
 }
